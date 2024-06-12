b4db859ca1cec85a1ce33601916e8f9d9610b53f
3759357 <94110438+3759357@users.noreply.github.com>
2024-06-13 03:20:32 +0900

    c
    메뉴 슬라이더 두개 적용, 게시판 이미지 기능, 이메일 기능, 초신선육 날짜

--- pigg/home/urls.py
+++ pigg/home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import *
 urlpatterns=[
     path('',views.main_feed),
     path('history/', views.history),
@@ -11,4 +11,9 @@ urlpatterns=[
     path('franchise_story/', views.franchise_story),
     path('franchise/', views.franchise),
     path('review/', views.review),
+    path('index/',views.index),
+    path('blog/',views.blog),
+    path('blog/<int:pk>/',views.posting, name="posting"),
+    path('story/<int:pk>/',views.posting, name="posting"),
+    path('review/send/', views.send_email),
 ]
--- pigg/home/views.py
+++ pigg/home/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView,DetailView
+from django.core.mail import EmailMessage
 
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+from django.http import HttpResponse
 # Create your views here.
 def main_feed(request):
     return render(
@@ -18,12 +27,12 @@ def history(request):
     )
 
 def story(request):
-    return render(
-        request,
-        'home/story.html'
-    )
+    postlist = Post.objects.all()
+    # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다
+    return render(request, 'home/story.html', {'postlist': postlist})
 
 def style(request):
+
     return render(
         request,
         'home/style.html'
@@ -59,3 +68,45 @@ def review(request):
         'home/review.html'
     )
 
+def index(request):
+    return render(request, 'home/blog.html')
+
+# blog.html 페이지를 부르는 blog 함수
+def blog(request):
+    postlist = Post.objects.all()
+    # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다
+    return render(request, 'home/blog.html', {'postlist': postlist})
+
+def posting(request, pk):
+    # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
+    post = Post.objects.get(pk=pk)
+    # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    return render(
+        request,
+        'home/posting.html',
+        {'post':post,}
+    )
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index2.html'
+    orderinf = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+
+def send_email(request):
+    inputReceiver = request.POST['title']
+    inputTitle = request.POST['name']
+    inputContent = request.POST['number']
+
+    content = {'inputReceiver': inputReceiver, 'inputTitle': inputTitle, 'inputContent': inputContent}
+
+    msg_html = render_to_string('home/dd.html', content)
+
+    msg = EmailMessage(subject=inputTitle, body=msg_html, from_email="wornjs6327@naver.com",
+                       bcc=inputReceiver.split(','))
+    msg.content_subtype = 'html'
+    msg.send()
+    return HttpResponseRedirect(reverse('dd'))
